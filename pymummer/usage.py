2172a74ac23ad16226f1d8b743d3de4b19b0f675
hwrn <hwrn.aou@sjtu.edu.cn>
2024-08-12 21:10:48 +0800

    feat: report_flattern_diff to use it

---
+++ pymummer/usage.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+ * @Date: 2024-08-12 17:23:26
+ * @LastEditors: hwrn hwrn.aou@sjtu.edu.cn
+ * @LastEditTime: 2024-08-12 21:09:18
+ * @FilePath: /pymummer/pymummer/usage.py
+ * @Description:
+"""
+# """
+
+from sys import stdout
+from .delta import DeltaRegion
+from .pair import Pair
+
+
+def report_flattern_diff(
+    flattern_align: dict[str, list[list[tuple[DeltaRegion, str]]]],
+    n_window=10,
+    min_diff=3,
+    stdout=stdout,
+):
+    write = lambda *x: print(*x, sep="\t", file=stdout)
+    write("#" + ">SeqID")
+    write("#" + "loc", "n_identical", "n_diff", "min_diff", "*aligns")
+    this: Pair.T = "ref"
+    for s in flattern_align:
+        contig = flattern_align[s][0][0][0].contig
+        if contig is not None and contig.seqid2["query"] == s:
+            this = "query"
+        break
+    other = Pair.switch(this)
+    for s in flattern_align:
+        write(f">{s}")
+        d10_used = {i: True for i in range(n_window)}
+        d10: dict[int, tuple[int, int, int, int, list[str]]] = {}
+        last_miss = -1
+        for basei, basesc in enumerate(flattern_align[s]):
+            d10[basei % n_window] = (
+                basei,
+                sum("|" in s for ali, s in basesc),
+                sum(s != "|" for ali, s in basesc),
+                min(
+                    list(len(s.replace("|", "").replace("+", "")) for ali, s in basesc)
+                    or [0],
+                ),
+                [
+                    (f"{ali.contig.seqid2[other]}{ali.loc2[other]}: {s}")
+                    for ali, s in basesc
+                    if ali.contig is not None
+                ],
+            )
+            d10_used[basei % n_window] = False
+            if (
+                sum(d10[i][2] for i in d10 if d10[i] is not None) >= min_diff
+                or sum(d10[i][3] for i in d10 if d10[i] is not None) >= min_diff
+                or sum(len(d10[i][4]) == 0 for i in d10 if d10[i] is not None)
+                >= min_diff
+            ):
+                for i in range(basei - n_window + 1, basei + 1):
+                    shift = i % n_window
+                    if d10_used[shift]:
+                        continue
+                    d10_used[shift] = True
+                    if len(d10[shift][4]) == 0:
+                        # since here, we have at least min_diff differences
+                        if last_miss == -1:
+                            last_miss = d10[shift][0]
+                        continue
+                    if last_miss != -1:
+                        write(f"{last_miss}...{d10[shift][0]-1}", 0)
+                        last_miss = -1
+                    write(*d10[shift][:-1], *d10[shift][-1])
+        if last_miss != -1:
+            write(f"{last_miss}...{d10[shift][0]-1}", 0)
